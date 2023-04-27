295480a5c20d8d31186c3d0d60aa788949089009
Antonio José Masiá <email@antoniomasia.com>
2023-04-27 15:57:39 +0200

    improve lualine plugin config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- style copilot icon
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("_colorscheme"),
+  callback = function()
+    local statusline_hl = vim.api.nvim_get_hl_by_name("StatusLine", true)
+
+    vim.api.nvim_set_hl(0, "SLCopilot", { fg = "#6CC644", bg = statusline_hl.background })
+  end,
+})
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,47 @@
+local null_ls = require("null-ls")
+
+local list_registered_providers_names = function(filetype)
+  local s = require("null-ls.sources")
+  local available_sources = s.get_available(filetype)
+  local registered = {}
+
+  for _, source in ipairs(available_sources) do
+    for method in pairs(source.methods) do
+      registered[method] = registered[method] or {}
+      table.insert(registered[method], source.name)
+    end
+  end
+  return registered
+end
+
+local formatters_list_registered = function(filetype, source_method)
+  local registered_providers = list_registered_providers_names(filetype)
+  return registered_providers[null_ls.methods[source_method]] or {}
+end
+
+local alternative_methods = {
+  null_ls.methods.DIAGNOSTICS,
+  null_ls.methods.DIAGNOSTICS_ON_OPEN,
+  null_ls.methods.DIAGNOSTICS_ON_SAVE,
+}
+
+local alternative_methods_list_registered = function(filetype)
+  local registered_providers = list_registered_providers_names(filetype)
+  local providers_for_methods = vim.tbl_flatten(vim.tbl_map(function(m)
+    return registered_providers[m] or {}
+  end, alternative_methods))
+
+  return providers_for_methods
+end
+
 return {
   {
+    -- {
+    --   "jonahgoldwastaken/copilot-status.nvim",
+    --   dependencies = { "zbirenbaum/copilot.lua" }, -- or "copilot.lua"
+    --   lazy = true,
+    --   event = "BufReadPost",
+    -- },
     {
       "nvim-lualine/lualine.nvim",
       event = "VeryLazy",
@@ -30,11 +72,14 @@ return {
               },
               { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
               { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
-          -- stylua: ignore
-          {
-            function() return require("nvim-navic").get_location() end,
-            cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
-          },
+              {
+                function()
+                  return require("nvim-navic").get_location()
+                end,
+                cond = function()
+                  return package.loaded["nvim-navic"] and require("nvim-navic").is_available()
+                end,
+              },
             },
             lualine_x = {
               {
@@ -64,8 +109,59 @@ return {
                   removed = icons.git.removed,
                 },
               },
+              -- {
+              --   require("copilot_status").status_string,
+              -- },
+              {
+                function(msg)
+                  msg = msg or "LS Inactive"
+
+                  local buf_clients = vim.lsp.get_active_clients()
+
+                  if next(buf_clients) == nil then
+                    -- TODO: clean up this if statement
+                    if type(msg) == "boolean" or #msg == 0 then
+                      return "LS Inactive"
+                    end
+                    return msg
+                  end
+
+                  local buf_ft = vim.bo.filetype
+                  local buf_client_names = {}
+                  local copilot_active = false
+
+                  -- -- add client
+                  for _, client in pairs(buf_clients) do
+                    if client.name ~= "null-ls" and client.name ~= "copilot" then
+                      table.insert(buf_client_names, client.name)
+                    end
+
+                    if client.name == "copilot" then
+                      copilot_active = true
+                    end
+                  end
+                  --
+                  -- -- add formatter
+                  local supported_formatters = formatters_list_registered(buf_ft, "FORMATTING")
+                  vim.list_extend(buf_client_names, supported_formatters)
+
+                  -- add linter
+                  local supported_linters = alternative_methods_list_registered(buf_ft)
+                  vim.list_extend(buf_client_names, supported_linters)
+
+                  local unique_client_names = vim.fn.uniq(buf_client_names)
+                  local language_servers = "[" .. table.concat(unique_client_names, ", ") .. "]"
+
+                  if copilot_active then
+                    language_servers = language_servers .. "%#SLCopilot#" .. " " .. "" .. "%*"
+                  end
+
+                  return language_servers
+                end,
+                color = { gui = "bold" },
+              },
+              { "filetype", cond = nil, padding = { left = 1, right = 1 } },
             },
-            lualine_y = { "filetype" },
             lualine_z = {
               { "progress", separator = " ", padding = { left = 1, right = 0 } },
               { "location", padding = { left = 0, right = 1 } },
